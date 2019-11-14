01accc2caafcb95adcbec19a92ee28e5769ef163
github username <github email address>
2019-11-13 18:54:43 -0800

    Added modals for edit and delete runner

--- runner-frontend/src/index.js
+++ runner-frontend/src/index.js
@@ -8,6 +8,9 @@
   addRunModal();
   newGoalModal();
   listenForRunSubmit();
+  addEventListenerDeleteAccount();
+  editAccountModal();
+  listenForEditSubmit()
   let runner;
 
   function showLoginPage() {
@@ -61,6 +64,10 @@
   function showMainPage() {
     document.getElementById('main-page').style.display = ""
     document.getElementById('login-page').style.display = "none"
+    const createModal = document.getElementById('modal-create')
+    createModal.style.display = 'none'
+    const loginModal = document.getElementById('modal-login')
+    loginModal.style.display = 'none'
   }
 
   //listen for login 
@@ -180,9 +187,10 @@
               name: runnerName
             })
           }).then(resp => resp.json())
-            .then(runner => {
-              renderRunner(runner);
+            .then(runnerr => {
+              renderRunner(runnerr);
               showMainPage();
+              runner = runnerr
             });
         }
       })
@@ -265,6 +273,70 @@
         goal_id: 1 // need to change this to current goal
       })
     }) // need to call on function to update goal status
+  }
+
+  function editAccountModal() {
+    const modal5 = document.getElementById("modal-edit");
+    const trigger5 = document.getElementById("trigger-edit");
+    const closeButton5 = document.getElementById("close-edit");
+
+    function toggleModal5() {
+      modal5.classList.toggle("show-modal");
+    }
+
+    function windowOnClick5(event) {
+      if (event.target === modal5) {
+        toggleModal5();
+      }
+    }
+
+    trigger5.addEventListener("click", toggleModal5);
+    closeButton5.addEventListener("click", toggleModal5);
+    window.addEventListener("click", windowOnClick5);
+  }
+
+  function listenForEditSubmit() {
+    const editForm = document.getElementById('edit')
+    editForm.addEventListener('submit', e => {
+      e.preventDefault()
+      const newName = e.target.name.value
+      patchToDatabase(newName);
+    })
+  }
+
+  function patchToDatabase(newName) {
+    fetch(`http://localhost:3000/runners/${runner.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: newName
+      })
+    }).then(resp => resp.json())
+    .then(runner => { 
+      renderRunner(runner)
+      const editModal = document.getElementById('modal-edit')
+      editModal.style.display = 'none'
+      editAccountModal()
+    })
+  }
+
+  function resetShowPage() {
+    showLoginPage();
+    createAccountModal();
+    loginModal();
+  }
+
+  function addEventListenerDeleteAccount() {
+    const deleteButton = document.getElementById('delete')
+    deleteButton.addEventListener('click', e => {
+      fetch(`http://localhost:3000/runners/${runner.id}`, {
+        method: 'DELETE'
+      }).then(resetShowPage())
+    })
+    
   }
 
 })();
